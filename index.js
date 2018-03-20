82342a3af3a0f45d4416dbe804bbfb9c2e713533
Kelin2025 <kelin2025@yandex.ru>
2018-03-21 01:26:17 +0300

    Added mixin

---
+++ index.js
@@ -0,0 +1,29 @@
+const defaultConfig = {
+  name: 'api',
+  isFetching: 'isFetching',
+  error: 'error',
+  result: 'result'
+}
+
+export const injectService = (service, config = defaultConfig) => ({
+  data: () => ({
+    [config.name]: {
+      started: false,
+      pending: false,
+      success: false,
+      error: null,
+      payload: null,
+      result: null
+    }
+  }),
+  beforeCreate() {
+    this[service.name] = service
+      .extend({ meta: { comp: this } })
+      .on('change:state', ({ nextState }) => {
+        this[config.name] = state
+      })
+  }
+})
+
+export const injectFromTree = tree => (service, config) =>
+  injectService(tree(service), conifg)
